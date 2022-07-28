65cee26859fc22dc6bc34f90564195d1ef4d48ca
Felipe Guell <feliguells@gmail.com>
2022-07-28 12:40:37 -0300

    first commit

---
+++ controllers/registro.controller.js
@@ -0,0 +1,19 @@
+import { clientServices } from "../service/client-service.js";
+
+
+const formulario = document.querySelector("[data-form]");
+
+
+formulario.addEventListener('submit', async (evento) =>{
+    evento.preventDefault();
+    const nombre = document.querySelector("[data-nombre]").value;
+    const email = document.querySelector("[data-email]").value;
+    
+    try{
+        const perfil = await clientServices.crearCliente(nombre,email);
+        window.location.href = "/screens/registro_completado.html";
+    }catch(error){
+        window.location.href ="/screens/error.html";
+    }
+    
+})
